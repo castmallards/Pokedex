d67c64da988976e4032c6510abf118cc44e166b5
castmallards <obaidali102@gmail.com>
2023-12-03 20:14:03 -0600

    Added another script
    Added scraper script for abilities and abilities.csv file.

--- Blueprints/__init__.py
+++ Blueprints/__init__.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask
 from .views import views # importing the views.py
-import sqlite3
-from sqlite3 import Error
+import pymssql
+from pymssql import Error
+import getpass
 
 # create flask app
 def create_app():
@@ -16,9 +17,31 @@ def create_connection(db_filename):
     ''' This creates a connection to the database using sqlite3
         :param db_filename: Database file name
         :return: Returns the conenection to database or None
-    '''    
-    conn = None
-
+    '''
+    try:
+        username = input("Enter your username\n")
+        password = getpass.getpass("Enter password\n")
+    except Exception as error:
+        print("Wrong input")
+    conn = pymssql.connect('uranium.cs.umanitoba.ca',username, password, 'cs3380')
+    conn.autocommit(True)
+    cursor = conn.cursor()
+    
+    '''
+        1) IF EXISTS, DROP ALL TABLES
+        2) CREATE THE TABLES
+        3) INSERT INTO TABLES
+    '''
+    # cursor.execute('CREATE table myTable(mynum int not Null, primary key (mynum))')
+    # cursor.execute('INSERT INTO myTable VALUES (1)')
+    # cursor.execute('INSERT INTO myTable VALUES (2)')
+    # cursor.execute('INSERT INTO myTable VALUES (3)')
+    # cursor.execute('INSERT INTO myTable VALUES (4)')
+    # cursor.execute('SELECT * from myTable')
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row)
+    
     try:
         conn = ""
     except Error as e:
@@ -27,3 +50,8 @@ def create_connection(db_filename):
     return None
 
 conn = create_connection("hello.txt")
+
+# import sql file
+# read sql file and store it in a variable
+#cur = conn.cursor()
+#cur.execute(sqlfile)
