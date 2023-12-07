15aefb21f8c630ff8f003e53161c60b1f7fc52ae
castmallards <obaidali102@gmail.com>
2023-12-07 16:53:39 -0600

    Making Interface

--- Blueprints/views.py
+++ Blueprints/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from flask.templating import render_template
-# from . import conn
+from . import conn 
 
 # :param: 
 # -- 'views' is name of file
@@ -16,8 +16,39 @@ def homepage():
 # Pokemon page shows a list of pokemons
 @views.route('/pokemon')
 def pokemon_page():
-    return render_template("pokemon.html")
+    curr = conn.cursor()
+    all_pok = 'SELECT * FROM Pokemon'
+    curr.execute(all_pok)
+    result = curr.fetchall()
+
+    return render_template("pokemon.html", Pokemon=result)
 
 @views.route('/moves')
 def moves_page():
     return render_template("moves.html")
+
+@views.route('/newPkmnIntro', methods=["GET"])
+def newPkmnIntro_page():
+
+    if request.method == "GET":
+        game = request.form['Games']
+
+    games_list = ''
+    types = ''
+    curr = conn.cursor()
+    curr.execute('SELECT ')
+
+    return render_template('newPkmnIntro.html', Games=games_list, Type=types)
+
+@views.route('/newPkmnIntro/<game_name>', methods=["GET"])
+def newPkmnIntro_page():
+
+    if request.method == "GET":
+        game = request.form['Games']
+
+    games_list = ''
+    types = ''
+    curr = conn.cursor()
+    curr.execute('SELECT ')
+
+    return render_template('newPkmnIntro_info.html', Games=games_list, Type=types)
